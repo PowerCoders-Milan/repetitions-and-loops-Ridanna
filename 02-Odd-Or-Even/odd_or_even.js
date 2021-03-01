cd2f13e8ed35bf6c55654c637beaeddc2b23f782
mahmudref@gmail.com <mahmudref@gmail.com>
2021-03-01 15:01:45 +0100

    exe2

--- 02-Odd-Or-Even/odd_or_even.js
+++ 02-Odd-Or-Even/odd_or_even.js
@@ -5,3 +5,18 @@
   // check if the current is even or odd
 
   // report the result to the screen
+
+
+for (i = 0; i < 21; i++) {
+  if (i === 0) {
+    console.log(i + " is even");
+  } else if (i % 2 === 0) {
+    console.log(i + " is even");
+  } else {
+    console.log(i + " is odd");
+  }
+}
+
+//while loop
+
+
