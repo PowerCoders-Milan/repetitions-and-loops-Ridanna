9e5fe29a43c56cb77d0fe9f6d070d90bb232825e
mahmudref@gmail.com <mahmudref@gmail.com>
2021-03-01 15:02:56 +0100

    exe3

--- 03-Multiplication-Tables/multiplication_tables.js
+++ 03-Multiplication-Tables/multiplication_tables.js
@@ -1,1 +1,17 @@
 // write your code below
+var multiply = '\n';
+for (var i = 1; i < 11; i++) {
+    for ( var n = 1; n < 11; n++) {
+       multiply += (i*n) + ' '; 
+    }
+    multiply += '\n'
+}
+
+var table = 9;
+    var count = 10;
+    
+    for (var i = 0; i < count; i++) {
+        i++;
+        console.log(table, '*', i, '=', table * i);
+        --i;
+    }
