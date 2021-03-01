355120096b233e49257949836972adf68e497e69
mahmudref@gmail.com <mahmudref@gmail.com>
2021-03-01 12:04:24 +0100

    exer1

--- 01-Arrays/arrays.js
+++ 01-Arrays/arrays.js
@@ -11,3 +11,25 @@ var array = ["Banana", "Apples", "Oranges", "Blueberries"];
 
 // Sort the array in reverse order, i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
 
+//access
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+console.log(array[2]);
+
+array.shift()
+console.log(array);
+
+array.sort()
+console.log(array);
+
+array.push("kiwi")
+console.log(array);
+
+array.splice(1, 1)
+console.log(array)
+
+array.reverse()
+console.log(array)
+
+
+
+
